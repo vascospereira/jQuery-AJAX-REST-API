609783b6e25203c768a9c6932faeadda1e8a2f3f
vascospereira <vascospereira@gmail.com>
2017-12-08 16:30:23 +0000

    some error handling

--- server.js
+++ server.js
@@ -9,6 +9,21 @@ app.use(bodyParser.json());
 
 app.use('/api', require('./routes/api'));
 
+app.use((req, res, next) => {
+    const error = new Error('Opss, Page Not Found!');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) =>{
+    res.status(error.status || 500);
+    res.json({
+        error:{
+            message: error.message
+        }
+    });
+});
+
 app.listen(PORT, function() {
     console.log('Server listening on ' + PORT);
 });
